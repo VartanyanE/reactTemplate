08d7cf00e7d1c230bb82b7af40e2ba24e60c2465
VartanyanE <mike.vartanyan@yahoo.com>
2022-07-20 16:51:58 -0700

    title

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,31 +1,47 @@
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 
+import logo from "../../assets/logo.svg";
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em",
+  },
+  logo: {
+    height: "7em",
+  },
+}));
+
 export default function Header(Props) {
-  function ElevationScroll(props) {
-    const { children } = props;
-
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0,
-    });
-
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
+  const classes = useStyles();
   return (
-    <ElevationScroll>
-      <AppBar color="primary">
-        <Toolbar>
-          <Typography variant="h4" color="secondary">
-            Manch Development
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed" color="primary">
+          <Toolbar disableGutters>
+            <img alt="company logo" className={classes.logo} src={logo} />
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+      Hello
+    </React.Fragment>
   );
 }
